b7052bc53c00f0d33e2bc879ba627b78fdbdb65c
anthony.dke@gmail.com <anthonydike@nyu.edu>
2023-01-03 11:36:50 -0500

    Create parent/child relationships and grandparent/child relationships btwn users, projects, subscriptions, and stakholder updates

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ApplicationRecord
   belongs_to :user
 
   has_many :stakeholder_updates
+  has_many :subscriptions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   scope :subscribed, -> { where(paying_customer: true) }
 
   has_many :projects
+  has_many :subscriptions
+  has_many :stakeholder_updates, through: :projects
 end
